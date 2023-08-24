9f945a36a14b3296c90ffe59a2c33a1dd939a2d2
Mateus Carvalho <mateus_raimundo95@outlook.com>
2023-08-24 17:42:37 -0300

    updated project

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,7 +1,21 @@
 import { Icon, Input, Stack } from "native-base";
 import { MagnifyingGlass } from "phosphor-react-native";
+import { useState } from "react";
 
 export function InputComponent() {
+  const [text, setText] = useState("");
+  const [focus, setFocus] = useState(false);
+
+  console.log(focus);
+
+  function handleFocus() {
+    setFocus(true);
+  }
+
+  function handleBlur() {
+    setFocus(false);
+  }
+
   return (
     <Stack space={4} w="100%" alignItems="center">
       <Input
@@ -13,12 +27,30 @@ export function InputComponent() {
         placeholderTextColor={"gray.300"}
         color="gray.500"
         fontFamily="body"
+        onFocus={handleFocus}
+        onBlur={handleBlur}
+        onChangeText={(text) => setText(text)}
         fontSize="text_sm"
         InputLeftElement={
-          <Icon
-            as={<MagnifyingGlass color="#8D8686" weight="regular" size={18} />}
-            ml="9.5"
-          />
+          text.length !== 0 ? (
+            <Icon
+              as={
+                <MagnifyingGlass color="#C47F17" weight="regular" size={18} />
+              }
+              ml="9.5"
+            />
+          ) : (
+            <Icon
+              as={
+                <MagnifyingGlass
+                  color={focus ? "#DBAC2C" : "#8D8686"}
+                  weight="regular"
+                  size={18}
+                />
+              }
+              ml="9.5"
+            />
+          )
         }
         placeholder="Pesquisar"
       />
